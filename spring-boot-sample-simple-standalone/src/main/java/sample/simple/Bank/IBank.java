14bea2987c80174d649c0328ed3bf72afe4a3e63
Le Goff Alban <alban.le-goff@etudiant.univ-rennes1.fr>
2021-09-21 16:16:30 +0200

    partie1 some change

--- spring-boot-sample-simple-standalone/src/main/java/sample/simple/Bank/IBank.java
+++ spring-boot-sample-simple-standalone/src/main/java/sample/simple/Bank/IBank.java
@@ -4,5 +4,6 @@ package sample.simple.Bank;
 public interface IBank {
     void getMoney();
 
-    boolean transfert();
+    //receive money from Bank account bank1
+    boolean transfert(IBank bank1, float ammount);
 }
