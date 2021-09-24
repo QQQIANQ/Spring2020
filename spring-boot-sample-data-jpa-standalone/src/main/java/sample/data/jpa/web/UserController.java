e74dc6574f173d71b9a70a4ec573e71f64b4aa36
maqian <maqian@hotmail.fr>
2021-09-24 09:17:44 +0200

    Partie3 page userList userInfo workerList added

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/UserController.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/UserController.java
@@ -1,10 +1,12 @@
 package sample.data.jpa.web;
 
+import javassist.NotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import sample.data.jpa.domain.User;
 import sample.data.jpa.service.UserDao;
 
@@ -19,19 +21,23 @@ public class UserController {
      */
     @RequestMapping(value = "/createuser", method = RequestMethod.POST)
     public String create(@RequestParam(value = "username", required = false) String username,
-                         @RequestParam(value = "dateNaissance", required = false) Date dateNaissance
+                         @RequestParam(value = "dateNaissance", required = false) Date dateNaissance,
+                         RedirectAttributes redirectAttributes
         ) {
         String userId = "";
         try {
             User user = new User(username,dateNaissance);
             userDao.save(user);
             userId = String.valueOf(user.getId());
+            redirectAttributes.addAttribute("id",user.getId());
         } catch (Exception ex) {
             return "Error creating the user: " + ex.toString();
         }
        // return "User succesfully created with id = " + userId;
-        return "redirect:/findallusers";
+        return "redirect:/userinfo";
     }
+
+
 
     @RequestMapping(value = "/getuser", method = RequestMethod.GET)
     @ResponseBody
@@ -47,6 +53,12 @@ public class UserController {
     }
 
 
+    @RequestMapping(value = "/userinfo")
+    public String userInfo(@RequestParam(value = "id", required = false) Long id,Model model) {
+        model.addAttribute("u",this.userDao.findUserById(id));
+        return "userInfo";
+    }
+
     @RequestMapping(value = "/findallusers", method = RequestMethod.GET)
     public String findAllUsers(Model model) {
         model.addAttribute("listUsers",this.userDao.findAll());
@@ -55,15 +67,25 @@ public class UserController {
     /**
      * GET /delete  --> Delete the user having the passed id.
      */
-    @RequestMapping(value = "/deleteuser", method = RequestMethod.DELETE)
-    @ResponseBody
-    public String delete(@RequestParam(value = "id", required = false) Long id) {
-        try {
-            userDao.delete(userDao.findUserById(id));
-        } catch (Exception ex) {
-            return "Error deleting the user:" + ex.toString();
-        }
-        return "User succesfully deleted!";
+    @GetMapping("/deleteuser/{id}")
+    //public String delete(@PathVariable("id") long id, Model model) {
+    public String delete(@PathVariable("id") long id, Model model) {
+        User user = userDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userDao.delete(user);
+        return "redirect:/";
+    }
+
+    /**
+     * GET /delete  --> Delete the user having the passed id.
+     */
+    @GetMapping("/deleteuseradmin/{id}")
+    //public String delete(@PathVariable("id") long id, Model model) {
+    public String deleteAdmin(@PathVariable("id") long id, Model model) {
+        User user = userDao.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("Invalid user Id:" + id));
+        userDao.delete(user);
+        return "redirect:/findallusers";
     }
 
 
--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/WorkerController.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/WorkerController.java
@@ -1,7 +1,22 @@
 package sample.data.jpa.web;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import sample.data.jpa.service.UserDao;
+import sample.data.jpa.service.WorkerDao;
 
 @Controller
 public class WorkerController {
+
+    @Autowired
+    private WorkerDao workerDao;
+
+    @RequestMapping(value = "/findallworkers", method = RequestMethod.GET)
+    public String findAllWorkers(Model model) {
+        model.addAttribute("listworkers",this.workerDao.findAll());
+        return "workerList";
+    }
 }
