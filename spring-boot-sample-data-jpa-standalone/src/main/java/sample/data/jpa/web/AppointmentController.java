07c8c472359d5e254ef0b62a3ab69869fe926eee
Le Goff Alban <alban.le-goff@etudiant.univ-rennes1.fr>
2021-10-15 19:32:36 +0200

    JPP

--- spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/AppointmentController.java
+++ spring-boot-sample-data-jpa-standalone/src/main/java/sample/data/jpa/web/AppointmentController.java
@@ -10,16 +10,22 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import sample.data.jpa.domain.Appointment;
 import sample.data.jpa.domain.User;
+import sample.data.jpa.domain.Worker;
 import sample.data.jpa.service.AppointmentDao;
 import sample.data.jpa.service.UserDao;
+import sample.data.jpa.service.WorkerDao;
 
 @Controller
 public class AppointmentController {
 	
 	@Autowired
     private AppointmentDao appointmentDao;
-	
+	@Autowired
+    private UserDao userDao;
+	@Autowired
+    private WorkerDao workerDao;
 	
 	@RequestMapping(value = "/findallappointments", method = RequestMethod.GET)
     public String findAllAppointments(Model model) {
@@ -30,27 +36,31 @@ public class AppointmentController {
 	
 	@RequestMapping(value = "/createappointment", method = RequestMethod.GET)
     public String createAppointmentGET(Model model) {
+		model.addAttribute("listUsers",this.userDao.findAll());
+		model.addAttribute("listWorkers",this.workerDao.findAll());
+		System.out.println(this.userDao.findAll().size());
         return "createAppointment";
     }
 	
 	
-	/*@RequestMapping(value = "/createAppointment", method = RequestMethod.POST)
-    public String createAppointmentPOST(@RequestParam(value = "username", required = false) String username,
-                         @RequestParam(value = "dateNaissance", required = false) Date dateNaissance,
-                         RedirectAttributes redirectAttributes
+	@RequestMapping(value = "/createappointment", method = RequestMethod.POST)
+    public String createAppointmentPOST(
+    			@RequestParam(value = "sujet") String sujet,
+                @RequestParam(value = "dateRDV") Date dateRDV,
+                @RequestParam(value = "userID") Long userId,
+                @RequestParam(value = "workerID") Long workerID
         ) {
-        String userId = "";
         try {
-            User user = new User(username,dateNaissance);
-            userDao.save(user);
-            userId = String.valueOf(user.getId());
-            redirectAttributes.addAttribute("id",user.getId());
+            User user = this.userDao.getOne(userId);
+            Worker worker = this.workerDao.getOne(workerID);
+
+            Appointment appointment= new Appointment(sujet,dateRDV,user,worker);
+            appointmentDao.save(appointment);
         } catch (Exception ex) {
-            return "Error creating the user: " + ex.toString();
+            return "Error creating the appointment: " + ex.toString();
         }
-       // return "User succesfully created with id = " + userId;
-        return "redirect:/userinfo";
-    }*/
+       return "Appointment succesfully created";
+    }
 
 	
 }
